08c1ae48ecd9916e80af79f983abc61b95c21dd0
Shoeb Iqrar <shoebiqrar6@gmail.com>
2024-03-27 20:29:10 +0530

    commitone

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js")
 const path = require("path");
 const methodOverride = require("method-override");
- const ejsMate = require("ejsmate");
-
-
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 main()
   .then(() => {
@@ -32,67 +33,19 @@ app.get("/", (req,res) => {
     res.send("Hi, I am root");
 });
 
-//Index Route
-app.get("/listings", async (req,res) => {
-  const allListings = await Listing.find({});
-  res.render("listings/index.ejs", {allListings});
-});
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews);
 
 
-//New Route
-app.get("/listings/new", (req,res) => {
-  res.render("listings/new.ejs");
-});
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "page not found!"));
+    });
 
-// show Route
-app.get("/listings/:id", async (req, res) => {
- let {id} = req.params;
- const listing = await Listing.findById(id);
- res.render("listings/show.ejs", { listing });
-});
-
-//Create Route
-app.post("/listings", async (req, res) => {
-let listing =req.body.listing;
-console.log(listing);
-});
-
- //Edit Route
- app.get("/listings/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/edit.ejs", { listing });
- });
-
- // Update Route
- app.put("/listings/:id", async (req,res) => {
-   let { id } = req.params;
-   await Listing.findByIdAndUpdate(id, {...req.body.listing});
-   res.redirect("/listings");
- });
-
- //Delete Route
- app.delete("/listing/:id", async (req, res) => {
-  let { id } = req.params;
-  let deletedListing = await Listing.findByIdAndDelete(id);
-  console.log(deletedListing)
-  res.redirect("/listings");
- });
-
-// app.get("/testListing", async (req, res) => {
-//      let sampleListing = new Listing({
-//         title: "My New Villa",
-//         description: "By the beach",
-//         price: 1200,
-//         location:"Calangute, Goa",
-//         country: "India",
-//     });
-
-
-
-    //  await sampleListing.save();
-    //  console.log("sample was saved");
-    //  res.send("successful testing");
+app.use((err, req,res,next)=> {
+  let { statusCode=500,message="something went wrong" } = err;
+  res.status(statusCode).render("error.ejs", { message });
+    //  res.status(statusCode).send(message);
+    });
 
 app.listen(8080, ()=> {
     console.log("server is listening to port 8080");
--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./reviews.js");
 
 const listingSchema = new Schema({
      title: {
@@ -9,13 +10,25 @@ const listingSchema = new Schema({
      description: String,
      image: {
       type:String,
-      default: "https://unsplash.com/photos/a-glass-walled-patio-with-a-table-and-chairs-Ye_5G755Ls0",
-      set: (v) => v === "" ? "https://unsplash.com/photos/a-glass-walled-patio-with-a-table-and-chairs-Ye_5G755Ls0" : v,
+      default: "https://images.unsplash.com/photo-1707343843344-011332037abb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      set: (v) => v === "" ? "https://images.unsplash.com/photo-1707343843344-011332037abb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
      },
      price: Number,
      location: String,
      country: String,
+     reviews: [
+       {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+       },
+     ],
 });
+
+ listingSchema.post("findOneAndDelete", async(listing) => {
+   if(listing) {
+   await Review.deleteMany({_id : { $in: listing.reviews } });
+   }
+ });
 
 const Listing = mongoose.model("Listing", listingSchema);  
 module.exports = Listing;
